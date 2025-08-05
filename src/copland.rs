81d53a27657982a426f5873d7688310dfd79ef15
Adam Petz <ampetz@ku.edu>
2025-08-05 02:11:41 -0500

    test

--- src/copland.rs
+++ src/copland.rs
@@ -279,6 +279,7 @@ fn do_AppraisalSummary_inner(et:EvidenceT, r:RawEvT, g:GlobalContext, s:Appraisa
             let et2_size = et_size(g.clone(), *et2.clone())?;
 
             let (r1, rest) = peel_n_rawev(et1_size, r)?;
+            print!("\nr1: {:?}", r1);
             let (r2, _) = peel_n_rawev(et2_size, rest)?;
 
             let s1 = do_AppraisalSummary_inner(*et1, r1.clone(), g.clone(), s)?;
